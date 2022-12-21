3f065ee2ab72c4128800cfd5b362127e1c379d09
ethanchen <ethan@fresho.com>
2022-12-21 20:41:35 +1100

    setup synlink for kitty

--- install.sh
+++ install.sh
@@ -4,4 +4,9 @@ DOTFILES=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
 rm -rf $HOME/.config/nvim
 ln -s $DOTFILES/nvim $HOME/.config/nvim
+
+rm -rf $HOME/.tmux.conf
 ln -s $DOTFILES/tmux.conf $HOME/.tmux.conf
+
+rm -rf $HOME/.config/kitty
+ln -s $DOTFILES/kitty $HOME/.config/kitty
